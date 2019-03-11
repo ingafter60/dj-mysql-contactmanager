8516d16017fe91eb38b1d55bf36fddf33109bbe9
ing <inyoman_gurnitha@yahoo.com>
2019-03-11 20:12:29 +0700

    15. Customizing Django Admin Site

--- app/admin.py
+++ app/admin.py
@@ -1,23 +1,17 @@
 from django.contrib import admin
-
-# Register your models here.
+from django.contrib.auth.models import Group
 from .models import Contact
 
-admin.site.register(Contact)
+## Register your models here.
 
-# from django.contrib import admin
-# from django.contrib.auth.models import Group
-# from .models import Contact
+# Display contact info in admin panel
+class ContactAdmin(admin.ModelAdmin):
+	list_display  = ('id', 'name', 'gender', 'email', 'info', 'phone')
+	list_display  = ('id', 'name', 'gender', 'email', 'info', 'phone')
+	list_editable = ('info',)
+	list_per_page = 3
+	search_fields = ('name', 'gender', 'email', 'info', 'phone')
+	list_filter   = ('gender', 'date_added')
 
-# ## Register your models here.
-
-# # Display contact info in admin panel
-# class ContactAdmin(admin.ModelAdmin):
-# 	list_display  = ('name', 'gender', 'email', 'info', 'phone')
-# 	list_editable = ('info',)
-# 	list_per_page = 3
-# 	search_fields = ('name', 'gender', 'email', 'info', 'phone')
-# 	list_filter   = ('gender', 'date_added')
-
-# admin.site.register(Contact, ContactAdmin)
-# admin.site.unregister(Group)
+admin.site.register(Contact, ContactAdmin)
+admin.site.unregister(Group)
