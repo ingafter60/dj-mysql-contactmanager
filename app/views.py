d3c862c7815242ef82a67513ec1e391024c3c6ef
ing <inyoman_gurnitha@yahoo.com>
2019-03-11 21:22:14 +0700

    19. Displaying contact objects in a template

--- app/views.py
+++ app/views.py
@@ -1,5 +1,12 @@
+# import django modules
 from django.shortcuts import render
+
+# import app files
+from .models import Contact
 
 # Create your views here.
 def home(request):
-	return render(request, 'index.html')
+	context = {
+		'contacts': Contact.objects.all()	
+	}	
+	return render(request, 'index.html', context)
